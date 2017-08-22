da450ae959597fef08c56f3f7a81748ff220ad80
Kyle Hughes <kyle.hughes@outlook.com>
2017-08-22 15:24:04 +0000

    Copy over most intermediate tasks

---
+++ tasks/intermediate/garage/garage.js
@@ -0,0 +1,45 @@
+class Vehicle {
+    constructor(kind, make, reg, faults) {
+        this.kind = kind
+        this.make = make
+        this.reg = reg
+        this.faults = faults
+    }
+    toString() {
+        return `${this.make} ${this.kind}: ${this.reg}`
+    }
+}
+
+function Garage(name){
+    this.name = name;
+
+    let vehicles = []
+    let updateListeners = []
+
+    this.getVehicle = function(index){
+        return vehicles[index]
+    }
+
+    this.addVehicle = function(vehicle){
+        // Push returns the new length, 
+        // so return the index of added vehicle
+        let ret =  vehicles.push(vehicle)-1
+        updateListeners.forEach(e=>e.call(null))
+        return ret
+    }
+
+    this.vehicles = function(){
+        return vehicles.slice()
+    }
+
+    this.addUpdateListener = function(cb){
+        updateListeners.push(cb)
+    }
+
+    this.removeVehicle = function(ind){
+        vehicles.splice(ind,1)
+        updateListeners.forEach(e=>e.call(null))
+    }
+
+    return this
+}
