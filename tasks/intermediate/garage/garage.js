53c0a328aa972cc53b01c7860d901122690f31ea
Kyle Hughes <kyle.hughes@outlook.com>
2017-08-22 15:37:01 +0000

    Formatting

--- tasks/intermediate/garage/garage.js
+++ tasks/intermediate/garage/garage.js
@@ -5,46 +5,46 @@ class Vehicle {
         this.reg = reg
         this.faults = faults
     }
-    getFixFee(){
+    getFixFee() {
         // Each line in the faults info is considered one fault
-        
-        let numlines = ((this.faults||'').match(/\n/g) || []).length
-        return numlines * ((this.kind==='Motorbike')?100:130)
+
+        let numlines = ((this.faults || '').match(/\n/g) || []).length
+        return numlines * ((this.kind === 'Motorbike') ? 100 : 130)
     }
     toString() {
         return `${this.make} ${this.kind}: ${this.reg}`
     }
 }
 
-function Garage(name){
+function Garage(name) {
     this.name = name;
 
     let vehicles = []
     let updateListeners = []
 
-    this.getVehicle = function(index){
+    this.getVehicle = function (index) {
         return vehicles[index]
     }
 
-    this.addVehicle = function(vehicle){
+    this.addVehicle = function (vehicle) {
         // Push returns the new length, 
         // so return the index of added vehicle
-        let ret =  vehicles.push(vehicle)-1
-        updateListeners.forEach(e=>e.call(null))
+        let ret = vehicles.push(vehicle) - 1
+        updateListeners.forEach(e => e.call(null))
         return ret
     }
 
-    this.vehicles = function(){
+    this.vehicles = function () {
         return vehicles.slice()
     }
 
-    this.addUpdateListener = function(cb){
+    this.addUpdateListener = function (cb) {
         updateListeners.push(cb)
     }
 
-    this.removeVehicle = function(ind){
-        vehicles.splice(ind,1)
-        updateListeners.forEach(e=>e.call(null))
+    this.removeVehicle = function (ind) {
+        vehicles.splice(ind, 1)
+        updateListeners.forEach(e => e.call(null))
     }
 
     return this
