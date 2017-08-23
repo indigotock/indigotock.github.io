e309182a3f75648bcfbdd3e9c1f69aef1e7cf063
Kyle Hughes <kyle.hughes@outlook.com>
2017-08-23 12:58:21 +0000

    Finish rektangles!!

--- tasks/advanced/rektangles.js
+++ tasks/advanced/rektangles.js
@@ -1,28 +1,9 @@
-let rev = function (s) {
-    let x = s.split('')
-    x.reverse()
-    return x.join('')
-}
-
 function getchar(s, x, y) {
-    s = rev(s)
     let l = s.length - 1
     if (x % l != 0 && y % l != 0)
         return ' '
-
-
-    let ind = 0
-    let axis = x
-    if (axis % l == 0)
-        axis = y
-
-    ind = Math.abs(l - (axis % l))
-
-    if ((axis % (l * 2)) > l)
-        s = rev(s)
-
-    let ret = s[ind] || '?'
-    if (ret == '?')
-        console.log(x, y, axis, ind)
-    return ret
+    let ind = (x + y) % (l * 2)
+    if (ind > l)
+        ind = l * 2 - ind
+    return s[ind]
 }
