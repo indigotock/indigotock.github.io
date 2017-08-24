ea5e5d6c872f9857ba55731fb1b5fb4d70f247be
Kyle Hughes <kyle.hughes@outlook.com>
2017-08-24 11:37:50 +0000

    Add meta to makedl and convert camelcase to normal form

--- tasks/intermediate/makedl.js
+++ tasks/intermediate/makedl.js
@@ -1,4 +1,11 @@
-function makedl(data) {
+let normalform = function (str) {
+    // Replace each capital with the captured character after a space
+    // Then uppercase the first character
+    return str.replace(/([A-Z])/g, ' $1')
+        .replace(/^./, (s) => s.toUpperCase())
+}
+
+function makedl(data, keymeta) {
     var dl = document.createElement('dl')
     let arraymode = (Array.isArray(data))
     let tagtype = 'dd'
@@ -11,15 +18,19 @@ function makedl(data) {
             let dt = document.createElement('dt')
             if (!isNaN(+key)) {
                 dt.textContent = Number(key) + 1
-            } else
-                dt.textContent = key + ':'
+            } else {
+                let newkey = key
+                if (keymeta && keymeta[key])
+                    newkey = keymeta[key]
+                dt.textContent = normalform(newkey) + ':'
+            }
             dl.appendChild(dt)
         }
         if (data.hasOwnProperty(key)) {
             let dd = document.createElement(tagtype)
             var element = data[key];
             if (typeof (element) == 'object') {
-                dd.appendChild(makedl(element))
+                dd.appendChild(makedl(element, keymeta))
             } else {
                 dd.textContent = element
             }
