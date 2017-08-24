89c02231434e82433fce326fb45aaed850ea5dda
Kyle Hughes <kyle.hughes@outlook.com>
2017-08-24 14:13:54 +0000

    Update rektangles to work with surrogate pairs

--- tasks/advanced/rektangles.js
+++ tasks/advanced/rektangles.js
@@ -1,9 +1,10 @@
 function getchar(s, x, y) {
+    s = new UnicodeString(s)
     let l = s.length - 1
     if (x % l != 0 && y % l != 0)
         return ' '
     let ind = (x + y) % (l * 2)
     if (ind > l)
         ind = l * 2 - ind
-    return s[ind]
+    return s.at(ind)
 }
